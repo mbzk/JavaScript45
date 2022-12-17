67f30420c41a0ec0ec99943edce45286b65c261c
mbzk <65045330+mbzk@users.noreply.github.com>
2022-12-18 01:26:15 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,112 @@
+// Question NO 02
+
+console.log("Question NO 02")
+
+var a = 'john';
+console.log("Hello " + a  + ", would you like to learn some python today?")
+
+// Question NO 03
+
+console.log("Question NO 03")
+
+var b = "jack";
+// lowercase
+console.log(b.toLowerCase()) 
+
+// upper Case
+console.log(b.toUpperCase())
+
+// title case
+// console.log(b.toTitleCase())
+
+
+// Question NO 04
+
+console.log("Question NO 04")
+
+var NameOfPerson = "unknown"
+var quote = '"Get comfortable with being uncomfortable"';
+console.log(NameOfPerson + " once said. " +  quote)
+
+// Question NO 05
+
+console.log("Question NO 05")
+
+var famous_person = "unknown Person";
+var message = '"what we know is drop what we dont know is an ocean"'
+console.log(famous_person + " once said " + message)
+
+
+// Question NO 06
+
+console.log("Question NO 06")
+
+var person_name = " \n\n MUHAMMAD BILAL ZAFAR KHAN \t";
+console.log(person_name);
+console.log(person_name.trim());
+
+
+// Question NO 07
+
+console.log("Question NO 07")
+
+// Addition
+var a = 5
+var b = 3
+
+var add = a + b
+console.log(add)
+
+// Subtraction
+var c = 10
+var d = 2
+
+var sub = c - d;
+console.log(sub)
+
+// Multiplication
+var e = 4;
+var f = 2;
+
+var multi = e * f;
+
+console.log(multi)
+
+
+// Division
+
+var g = 16;
+var h = 2;
+
+var div = 16/2;
+
+console.log(div)
+
+
+
+// Question NO 8
+
+console.log("Question NO 08")
+
+console.log(6+2);
+console.log(10-2);
+console.log(4*2);
+console.log(16/2);
+
+
+// Question NO 9 
+
+console.log("Question NO 09")
+
+fav_num = 2;
+console.log("your favourite number is " + fav_num)
+
+// Question NO 10
+
+console.log("Question NO 10")
+
+var person_name = "Bilal";      //storing Student name in a variable
+var age = 22;            //storing Student age in a variable
+
+console.log("The Student name is " + person_name + " and his age is " + age );      // printing the statement
+
